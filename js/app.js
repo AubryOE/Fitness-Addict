7eaab15fe58c2ab47bfa29b8b2a7c1e75232cdf4
Aubry Oehlberg <aubry.oehlberg@gmail.com>
2019-07-09 20:31:33 -0500

    Adding hamburger dropdown menu for smaller viewports

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-// alert("js file connected");
+// fetch("./header.html")
+//   .then(response => {
+//     return response.text()
+//   })
+//   .then(data => {
+//     document.querySelector(".header").innerHTML = data;
+//   });
 
-fetch("./header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector(".header").innerHTML = data;
-  });
+//Based on Semantic UI Responsiveness Menu by Designosis: https://codepen.io/designosis/pen/LbMgya
+
+$('.ui.sidebar').sidebar({
+    context: $('.ui.pushable.segment'),
+    transition: 'overlay'
+}).sidebar('attach events', '#mobile_item');
+
+$('.ui.vertical.sidebar').sidebar({
+    'onHidden': () => {
+         $('.pusher').css('overflow', 'initial');
+}
+}).sidebar()
